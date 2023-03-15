b1fdae612a006040de213f1146029dfc4e865e75
Gor <7495500@gmail.com>
2023-03-15 21:17:17 +0300

    Create projects and tasks with scaffold

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,51 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[ show edit update destroy ]
+
+  def index
+    @projects = Project.all
+  end
+
+  def show; end
+  def edit; end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to project_url(@project) }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to project_url(@project) }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_url }
+    end
+  end
+
+  private
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description, :completed)
+  end
+end
